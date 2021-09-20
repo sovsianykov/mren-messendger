b0bc81b7c829a2c7788325017138c70789ec5b3b
”sovsianykov” <swebproger@gmail.com>
2021-09-20 18:07:22 +0300

    first5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import {  FormControl, Input } from '@material-ui/core'
 import Message from './Message';
 import FlipMove from 'react-flip-move';
